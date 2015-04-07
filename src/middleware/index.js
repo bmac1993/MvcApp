7a8769508d3125b0c63103badcf78fb85e53ded9
Ren <renardchien@gmail.com>
2015-04-07 13:47:35 -0400

    Adding in session hooks

---
+++ src/middleware/index.js
@@ -0,0 +1,30 @@
+
+var requiresLogin = function(req, res, next) {
+
+};
+
+var requiresLogout = function(req, res, next) {
+
+};
+
+var requiresSecure = function(req,res,next) {
+
+};
+
+var bypassSecure = function(req, res, next){
+
+}
+
+module.exports.requiresLogin = requiresLogin;
+module.exports.requiresLogout = requiresLogout;
+
+
+
+
+
+
+
+
+
+
+
