adf7e572750aa0263feba3cf7a3fa0ed02024cb2
bmac1993 <bmac1993@gmail.com>
2015-04-23 14:53:48 -0400

    test

--- src/controllers/Cart.js
+++ src/controllers/Cart.js
@@ -1,6 +1,6 @@
 ﻿var _ = require('underscore');
 var models = require('../models');
-var CartItem = models.Item.ItemSchema;
+var CartItem = models.Item.ItemModel;
 var Items = models.Items.Items;
 
 var show = function (req, res) {
